f3ccaa4c5756571fcb7c181566db16cfeaa89f7d
taito11 <>
2023-09-23 13:10:26 +0900

    - home画面を追加する

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="home" />
+
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-white-500 text-white">
+            Dashboard</Link>
+
+            <template v-else>
+                <Link :href="route('login')"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-white-500 text-white">
+                Log in</Link>
+
+                <Link v-if="canRegister" :href="route('register')"
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-white-500 text-white">
+                Register</Link>
+            </template>
+        </div>
+        <div class="w-screen h-screen flex flex-col items-center justify-center bg-blue-500 text-white">
+            <h1 class="text-8xl font-extrabold">自分レポート</h1>
+            <h2 class="text-2xl font-semibold mt-8">自分レポートでは、今日の自分がどうであったかを<br>レポートとして記録していくアプリです。</h2>
+        </div>
+    </div>
+</template>
+<style></style>
